a53a3723f9ef6b738ebb6fdcd34ff34e5188cedf
himanshu sharma <himanshu.sharma@shephertz.co.in>
2013-10-29 18:13:19 +0530

    1.0 Client artifacts

---
+++ 1.0/Test_CSharp_SDK/UserServiceSample.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.shephertz.app42.paas.sdk.csharp;
+using com.shephertz.app42.paas.sdk.csharp.user;
+
+
+namespace Test_CSharp_SDK
+{
+    class UserServiceSample
+    {
+        ///<summary> 
+        ///Main Method To Create App42 User on Cloud.
+        ///</summary>
+        ///<param name=args>args</param>
+
+        static void Main(string[] args)
+        {
+            CreateUser();
+        }
+        /// <summary>
+        /// Test Method for creating the User in App42 Cloud. 
+        /// </summary>
+        public static void CreateUser()
+        {
+            /// Enter your Public Key and Private Key Here in Constructor. You can
+            /// get it once you will create a app in app42 console.
+
+            ServiceAPI sp = new ServiceAPI("YOUR_API_KEY","YOUR_SECRET_KEY");
+
+            /// Create Instance of User Service
+            UserService userService = sp.BuildUserService();
+            /// Create user or call other available method on the user service
+            /// reference
+            try
+            {
+                User user = userService.CreateUser("Your User Name","Your PassWord","Your EmailId");
+                Console.WriteLine("Your UserName is " + user.GetUserName());
+                Console.WriteLine("Your Password is " + user.GetEmail());
+            }
+            catch (App42BadParameterException ex)
+            {
+
+                /// Exception Caught
+                /// Check if User already Exist by checking app error code
+                if (ex.GetAppErrorCode() == 2001)
+                {
+                    Console.WriteLine("App42BadParameterException : " + ex.GetAppErrorCode());
+                    /// Do exception Handling for Already created User.
+                }
+                if (ex.GetAppErrorCode() == 2005)
+                {
+                    Console.WriteLine("App42BadParameterException : " + ex.GetAppErrorCode());
+                    /// Do exception Handling for Already created User.
+                }
+
+            }
+            catch (App42SecurityException ex)
+            {
+                /// Exception Caught
+                /// Check for authorization Error due to invalid Public/Private Key
+                if (ex.GetAppErrorCode() == 1401)
+                {
+                    /// Do exception Handling here
+                    Console.WriteLine("App42SecurityException : " + ex.GetAppErrorCode());
+                }
+            }
+            catch (App42Exception ex)
+            {
+                Console.WriteLine("App42Exception : " + ex);
+            }
+            /// Render the object response. This will return the Successful created
+            /// User response
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 App42_CSHARP_SDK
 ================
 
-App42 Cloud API Client SDK files for C#
+App42 BPaaS Cloud API Client SDK files for CSHARP
 
-[Download the latest App42 C# SDK] (https://github.com/shephertz/App42_CSHARP_SDK/raw/master/1.7/app42_csharp_sdk_1.7.zip)
+- Download the App42 BPaaS CSHARP SDK
+- Unzip the file and open **App42 CSHARP Sample** project.
+- Add the **App42_BPaaS_CSharp_SDK_x.x.x.dll** into refrence. (**Add Reference -> Select Your dll path)**
+- Initialize the library using
+```
+ServiceAPI sp = new ServiceAPI("<YOUR_API_KEY>","<YOUR_SECRET_KEY>");
+sp.SetBaseURL("<YOUR_API_SERVER_URL>");
+```
+- Instantiate the service that one wants to use in the App, e.g. using User service one has to do the following
+```
+UserService userService = sp.BuildUserService();
+```
 
-[Documentation and API guide] (http://api.shephertz.com/app42-dev/csharp-backend-apis.php)
+- Now one can call associated method of that service e.g. user creation can be done with the following snippet
+
+```
+String userName = "Nick";
+String pwd = "********";
+String emailId = "nick@shephertz.co.in";    
+User user = userService.CreateUser(userName, pwd, emailId); 
+Console.WriteLine("userName is " + user.GetUserName());
+Console.WriteLine("emailId is " + user.GetEmail());
+```
+
+- Build the project and run.
